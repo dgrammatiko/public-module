894eef5b28c0c64a4745a2aa4a10bf24946a72bf
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-05-06 12:04:43 +0200

    less is more

--- src/modules/administrator/publicfolder/src/Helper/PublicfolderHelper.php
+++ src/modules/administrator/publicfolder/src/Helper/PublicfolderHelper.php
@@ -25,7 +25,7 @@ class PublicfolderHelper
       $folder = rtrim($input->getString('folder', ''), '/');
       $folder = rtrim($folder, '\\');
 
-      self::createPublicFolder($folder);
+      return self::createPublicFolder($folder);
     }
 
     throw new LogicException('You shall not pass', 200);
@@ -72,10 +72,8 @@ class PublicfolderHelper
       }
     }
 
-    // Create the index.php both for root, api and administrator
-    self::createFile($folder . '/index.php', file_get_contents(JPATH_ROOT . '/index.php'));
-    self::createFile($folder . '/administrator/index.php', file_get_contents(JPATH_ROOT . '/index.php'));
-    self::createFile($folder . '/api/index.php', file_get_contents(JPATH_ROOT . '/api/index.php'));
+    // Copy static files
+    file_put_contents($folder . '/includes/incompatible.html', file_get_contents(JPATH_ROOT . '/templates/system/incompatible.html'));
 
     // Copy the robots
     if (is_file(JPATH_ROOT . '/robots.txt')) {
@@ -91,19 +89,59 @@ class PublicfolderHelper
       self::createFile($folder . '/htaccess.txt', file_get_contents(JPATH_ROOT . '/htaccess.txt'));
     }
 
+    // Create the index.php both for root, api and administrator
+    self::createFile($folder . '/index.php', file_get_contents(JPATH_ROOT . '/index.php'));
+    self::createFile($folder . '/administrator/index.php', file_get_contents(JPATH_ROOT . '/index.php'));
+    self::createFile($folder . '/api/index.php', file_get_contents(JPATH_ROOT . '/api/index.php'));
+
+    $definesTemplate = <<<HTML
+<?php
+
+/**
+ * @package    Joomla.Site
+ *
+ * @copyright  (C) 2005 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+// Defines.
+define('JPATH_BASE', {{BASEFOLDER}});
+define('JPATH_ROOT', JPATH_BASE);
+define('JPATH_SITE', JPATH_ROOT);
+define('JPATH_PUBLIC', {{PUBLICOLDER}});
+define('JPATH_CONFIGURATION', JPATH_ROOT);
+define('JPATH_ADMINISTRATOR', JPATH_ROOT . DIRECTORY_SEPARATOR . 'administrator');
+define('JPATH_LIBRARIES', JPATH_ROOT . DIRECTORY_SEPARATOR . 'libraries');
+define('JPATH_PLUGINS', JPATH_ROOT . DIRECTORY_SEPARATOR . 'plugins');
+define('JPATH_INSTALLATION', JPATH_ROOT . DIRECTORY_SEPARATOR . 'installation');
+define('JPATH_THEMES', JPATH_BASE . DIRECTORY_SEPARATOR . 'templates');
+define('JPATH_CACHE', JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'cache');
+define('JPATH_MANIFESTS', JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'manifests');
+define('JPATH_API', JPATH_ROOT . DIRECTORY_SEPARATOR . 'api');
+define('JPATH_CLI', JPATH_ROOT . DIRECTORY_SEPARATOR . 'cli');
+define('_JDEFINES', '1');
+
+HTML;
+
+    // The defines file
+    $definesContent = str_replace('{{BASEFOLDER}}', '"' . JPATH_BASE . '"', $definesTemplate);
+    $definesContent = str_replace('{{PUBLICFOLDER}}', '"' . $folder . '"', $definesContent);
+
     // Populate the includes
+    self::createFile($folder . '/defines.php', $definesContent);
     self::createFile($folder . '/includes/app.php', file_get_contents(JPATH_ROOT . '/includes/app.php'));
-    self::createFile($folder . '/includes/defines.php', file_get_contents(JPATH_ROOT . '/includes/defines.php'));
     self::createFile($folder . '/includes/framework.php', file_get_contents(JPATH_ROOT . '/includes/framework.php'));
 
     // Populate the administrator/includes
+    self::createFile($folder . '/administrator/defines.php', $definesContent);
     self::createFile($folder . '/administrator/includes/app.php', file_get_contents(JPATH_ROOT . '/administrator/includes/app.php'));
-    self::createFile($folder . '/administrator/includes/defines.php', str_replace("'JPATH_PUBLIC', JPATH_ROOT", "'JPATH_PUBLIC', $folder", file_get_contents(JPATH_ROOT . '/administrator/includes/defines.php')));
     self::createFile($folder . '/administrator/includes/framework.php', file_get_contents(JPATH_ROOT . '/administrator/includes/framework.php'));
 
     // Populate the api/includes
+    self::createFile($folder . '/api/defines.php', $definesContent);
     self::createFile($folder . '/api/includes/app.php', file_get_contents(JPATH_ROOT . '/api/includes/app.php'));
-    self::createFile($folder . '/api/includes/defines.php', str_replace("'JPATH_PUBLIC', JPATH_ROOT", "'JPATH_PUBLIC', $folder", file_get_contents(JPATH_ROOT . '/api/includes/defines.php')));
     self::createFile($folder . '/api/includes/framework.php', file_get_contents(JPATH_ROOT . '/api/includes/framework.php'));
 
     self::createFile(dirname(dirname(__DIR__)) . '/engaged.json', '{ "publicPath": "'. $folder . '" }');
