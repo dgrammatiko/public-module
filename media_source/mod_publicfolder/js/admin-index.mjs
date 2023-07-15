d0d92a8468ca234354225b9ed2b9a8ae17105f91
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-07-15 17:52:51 +0200

    remove redundant

--- media_source/mod_publicfolder/js/admin-index.mjs
+++ media_source/mod_publicfolder/js/admin-index.mjs
@@ -12,30 +12,35 @@ const renderMessage = (type, msg) => {
   button.addEventListener('click', async (event) => {
     let resp;
     const el = event.currentTarget;
-    el.setAttribute('disabled', '');
     const url = new URL(`${el.dataset.url}index.php?option=com_ajax&format=json&module=publicfolder&method=create&folder=${input.value}`);
     if (!url) return;
+
+    el.setAttribute('disabled', '');
 
     try {
       resp = await fetch(url, { method: 'POST', headers: { 'X-CSRF-Token': Joomla.getOptions('csrf.token') || '' } });
     } catch (err) {
       renderMessage('error', ['Internal error']);
+      el.removeAttribute('disabled');
     }
 
     if (resp.status !== 200) {
       renderMessage('error', ['Internal error']);
+      el.removeAttribute('disabled');
       return;
     }
+
     const respJson = await resp.json();
-    if (respJson.error) {
-      renderMessage('error', [respJson.message]);
-    } else {
-      renderMessage('success', [respJson.message]);
+    if (!respJson) {
+      renderMessage('error', ['Internal error']);
+      el.removeAttribute('disabled');
+      return;
     }
+    renderMessage(respJson.error ? 'error' : 'success', [respJson.message]);
     el.removeAttribute('disabled');
     input.form.classList.add('visually-hidden');
     input.form.nextElementSibling.classList.remove('visually-hidden');
-    input.form.nextElementSibling.innerHTML = input.form.nextElementSibling.innerHTML.replace('{{path}}', input.value);
+    input.form.nextElementSibling.innerHTML = input.form.nextElementSibling.innerHTML.replace('{{path}}', Joomla.sanitizeHtml(input.value));
   });
   button.removeAttribute('disabled');
 });
